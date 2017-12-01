3043e9661089698c9de65cb7b821053ecab2c2d5
Lovas Bálint <lovasb93@gmail.com>
2017-12-01 11:17:35 +0100

    hozzáad logika elkészítve

--- public_html/js/todo.js
+++ public_html/js/todo.js
@@ -2,3 +2,25 @@
  * @author Lovas Bálint <lovasb93@gmail.com>
  */
 
+$(document).ready(function() {
+    $("#add").on('click', function() {
+        $("#form").submit(function(e){
+            e.preventDefault();
+        });
+        addTodo();
+    });
+    
+    function addTodo() {
+        var todo = $("#todo").val();
+        if(isEmpty(todo))
+            $("#list").append("<li class=\"list-group-item\">" + todo + "</li>");
+        $("#todo").val("");
+    }
+    
+    function isEmpty(variable) {
+        if(variable !== "")
+            return true;
+        else
+            return false;
+    }
+});
